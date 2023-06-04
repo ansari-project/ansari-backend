187f0ea0ef4be3f67625d83cc652d2a091cc1e7a
M Waleed Kadous <mwk@anyscale.com>
2023-06-04 02:30:07 -0700

    Limit concurrency to fix bug.

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ from langchain.schema import (
     LLMResult,
     SystemMessage
 )
+import uuid
 
 END = '-- END --'
 GREETING ="""Assalamu alaikum! My name is Ansari. I can help you with your questions about Islam.
@@ -34,8 +35,9 @@ But I still get things wrong sometimes. It is always best to consult a real Isla
 
 with gr.Blocks() as demo:
     class MyCBH(BaseCallbackHandler):
-        def __init__(self, q):
+        def __init__(self, q, my_id):
             self.q = q
+            self.my_id = my_id
 
         def on_llm_new_token(
             self,
@@ -45,17 +47,19 @@ with gr.Blocks() as demo:
             parent_run_id = None,
             **kwargs,
         ) -> None:
-            self.q.put(token)
+            print(f'My_id is {my_id} for token {token}')
+            self.q.put((token,my_id))
         
         def on_llm_end(self, response, *, run_id, parent_run_id, **kwargs):
             self.q.put(END)
 
+    my_id = str(uuid.uuid4())
     history = [["", GREETING]]
     chatbot = gr.Chatbot(value=history)
     msg = gr.Textbox() 
     q = SimpleQueue()
     oai = PromptLayerChatOpenAI(streaming=True, temperature=0, model_name='gpt-3.5-turbo',
-                    callbacks=[MyCBH(q)], pl_tags=['ansari'])
+                    callbacks=[MyCBH(q, my_id)], pl_tags=[f'ansari-{my_id}'])
     openai_history = [SystemMessage(content="""
     You are a helpful assistant. Your name is Ansari. You help Muslims become stronger in my faith. 
     Respond to questions with information drawn from the Hadith 
@@ -72,14 +76,17 @@ with gr.Blocks() as demo:
         openai_history.append(HumanMessage(content=user_message))
         return gr.update(value="", interactive=False), history + [[user_message, None]]
 
-    async def bot(history):
+    def bot(history):
         #print(f'History is {history}')
         history[-1][1] =''
         thread =  Thread(target = oai.predict_messages, kwargs = {'messages': openai_history})
         thread.start() 
         token = q.get() # Get rid of crap token. 
         while True: 
-            token = q.get()
+            token, qid = q.get()
+            # print(f'Got {token} with {my_id} and {qid}')
+            if(qid != my_id):
+                print(f'!!!! Mismatched on {token}')
             if token == END:
                break
             history[-1][1] += token
@@ -92,5 +99,5 @@ with gr.Blocks() as demo:
         bot, chatbot, chatbot
     ).then(lambda: gr.update(interactive=True), None, [msg], queue=False)
 
-demo.queue(concurrency_count=16)
+demo.queue(concurrency_count=1)
 demo.launch()
