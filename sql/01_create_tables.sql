bd1a705ab455d72e5e4ec5a51e2817c345638139
M Waleed Kadous <waleedk@gmail.com>
2024-01-09 18:07:28 -0800

    Comments + add dependency on psycopg2.

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -1,10 +1,9 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
+    email VARCHAR(100) UNIQUE, -- can be null if it is a guest account
+    password_hash VARCHAR(255), -- can be null if it is a guest account
+    first_name VARCHAR(50), -- can be null if it is a guest account
+    last_name VARCHAR(50), -- can be null if it is a guest account
     preferred_language VARCHAR(10) DEFAULT 'en',
     is_guest BOOLEAN NOT NULL DEFAULT FALSE, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
