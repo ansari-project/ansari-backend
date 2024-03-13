0e9cd1578e20c54967907dec0a7fc0d7cd6b1e41
abdullah-alnahas <abdullah.nahass@gmail.com>
2024-03-13 13:02:09 +0300

    Remove unnecessary GPT4 answer quality testing

--- tests/test_answer_quality.py
+++ tests/test_answer_quality.py
@@ -2,7 +2,6 @@ import json
 import pytest
 import logging
 from logging import StreamHandler
-import litellm
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 
@@ -60,26 +59,6 @@ def is_correct(row):
     return clean_correct == clean_prediction
 
 
-def answer_question_gpt4(question, q_temp):
-    LOGGER.info(f'Answering question: {question["question"]}')
-    options = [o.strip() for o in question["options"].split(",")]
-    prompt = q_temp.render(question=question["question"], options=options)
-    response = litellm.completion(
-        model="gpt-4-1106-preview",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt},
-        ],
-        timeout=30.0,
-        temperature=0.0,
-        metadata={"generation-name": "ansari"},
-        num_retries=5,
-    )
-    result = response.choices[0].message
-    LOGGER.info(f"Answer: {result}")
-    return result
-
-
 def test_ansari_agent(data):
     df, q_temp, cache = data
     df["json_prediction"] = df.apply(
@@ -102,24 +81,3 @@ def test_ansari_agent(data):
     assert (
         correct_percentage >= 95
     ), f"Correct predictions percentage ({correct_percentage:.2f}%) is less than 95%"
-
-    # df["gpt4_prediction"] = df.apply(
-    #     lambda row: answer_question_gpt4(row, q_temp), axis=1
-    # )
-
-    # df["predicted_gpt4"] = df.apply(extract_prediction, axis=1)
-
-    # df["gpt4_correct_prediction"] = df.apply(is_correct, axis=1)
-    # gpt4_correct_percentage = df["gpt4_correct_prediction"].mean() * 100
-    # LOGGER.info(
-    #     f"\nPercentage of GPT-4 correct predictions: {gpt4_correct_percentage:.2f}%"
-    # )
-
-    # wrong_gpt4_predictions = df[df["gpt4_correct_prediction"] == False]
-    # if not wrong_gpt4_predictions.empty:
-    #     LOGGER.info("\nQuestions with wrong GPT-4 predictions:")
-    #     for index, row in wrong_gpt4_predictions.iterrows():
-    #         LOGGER.info(f"Question: {row['question']}")
-    #         LOGGER.info(f"Correct Answer: {row['correct']}")
-    #         LOGGER.info(f"Predicted Answer: {row['predicted_gpt4']}")
-    #         LOGGER.info("---------------------------------------")
