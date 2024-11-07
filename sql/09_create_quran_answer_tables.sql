3a1453d73fb85e121ae0d523a4ad4ae04d5450a0
abdullah-alnahas <abdullah.nahass@gmail.com>
2024-11-07 16:58:57 +0300

    feat(09_create_quran_answer_tables.sql): create a table for quran.com integ

---
+++ sql/09_create_quran_answer_tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE reviewers ( 
+   id SERIAL PRIMARY KEY, 
+   name VARCHAR(255) NOT NULL 
+);
+
+CREATE TABLE quran_answers (
+  id SERIAL PRIMARY KEY, 
+  surah INT, 
+  ayah INT, 
+  question TEXT, 
+  ansari_answer TEXT, 
+  review_result VARCHAR(20) CHECK (review_result IN ('pending', 'approved', 'edited', 'disapproved')),
+  reviewed_by INT, 
+  reviewer_comment TEXT,
+  final_answer TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (reviewed_by) REFERENCES reviewers(id)
+);
