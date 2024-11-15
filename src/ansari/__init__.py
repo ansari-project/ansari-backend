4698498f77871d1a8ebf7bf8cc998e74684c2684
abdullah-alnahas <abdullah.nahass@gmail.com>
2024-11-15 23:08:28 +0300

    fix(src/ansari/__init__.py): make ansari_logger module accessible

--- src/ansari/__init__.py
+++ src/ansari/__init__.py
@@ -1,5 +1,5 @@
 # This file marks the directory as a Python package.
 from .config import Settings, get_settings
-import ansari_logger
+from . import ansari_logger
 
 __all__ = ["Settings", "get_settings", "ansari_logger"]
